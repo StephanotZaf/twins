82a371d92afd17d1f1eaf77aa4eecc2e83277557
Stephanot <stephanotkr@gmail.com>
2022-01-22 17:09:35 +0300

    refactoring

--- twins.php
+++ twins.php
@@ -1,34 +1,8 @@
 <?php
 
-function is_twins(string $original ,string $toCompare): bool{
-    if (empty($original !== '' || empty($toCompare)))
-    {
-        return false;
-    }
-    if ( strlen($original) != strlen($toCompare))
-    {
-        return false;
-    }
-
-    $originalArray = count_chars(strtolower($original), 1);
-    $toCompareArray = count_chars(strtolower($toCompare), 1);
-
-    if (count($originalArray) != count($toCompareArray)) {
-        return false;
-    }
-
-    if (count($originalArray) == count($toCompareArray)) {
-//        $sortedOriginKey = ksort($originalArray);
-//        $sortedCompareKey = ksort($toCompareArray);
-        if ($originalArray === $toCompareArray) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    return false;
-
+function is_twins(string $original ,string $toCompare): bool
+{
+    return (count_chars(strtolower($original), 1) === count_chars(strtolower($toCompare), 1));
 }
-
 $value = is_twins('Onnaaann', 'Aaaonnnn');
 var_dump($value);
