efc0b88271f128d847f4c0fc004b703b92bbfde8
RAZAFIMANDIMBY Niaina Michaël (balzacLeGeek) <michaniainar@gmail.com>
2022-01-22 16:41:04 +0300

    Compare count_chars

--- twins.php
+++ twins.php
@@ -1,30 +1,8 @@
 <?php
 
-function is_twins(string $original ,string $toCompare){
-    if ( strlen($original) != strlen($toCompare))
-    {
-        echo 'The two string has not same length';
-    }
-
-    $originalArray = count_chars($original, 1);
-    $toCompareArray = count_chars($toCompare, 1);
-
-    if (count($originalArray) != count($toCompareArray)) {
-        echo 'Character grouped is not same length';
-    }
-
-    if (count($originalArray) == count($toCompareArray)) {
-//        $sortedOriginKey = ksort($originalArray);
-//        $sortedCompareKey = ksort($toCompareArray);
-        if ($originalArray === $toCompareArray) {
-            var_dump($originalArray);
-            var_dump($toCompareArray);
-            echo 'twins';
-        } else {
-            echo 'not twins but contains same number of characters and same number of grouped caracters';
-        }
-    }
-
+function is_twins(string $original ,string $toCompare): bool
+{
+    return count_chars(strtolower($a), 1) === count_chars(strtolower($b), 1);
 }
 
 is_twins('nonaaann', 'aaaonnnn');
