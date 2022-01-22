cbfa9eced126d134b9ae339d93cd722108b4ee85
Stephanot <stephanotkr@gmail.com>
2022-01-22 17:13:26 +0300

    add exception when nullable variable

--- twins.php
+++ twins.php
@@ -2,7 +2,11 @@
 
 function is_twins(string $original ,string $toCompare): bool
 {
+    if (empty($original) || empty($toCompare))
+    {
+        throw new Exception('Two variables must non nullable');
+    }
     return (count_chars(strtolower($original), 1) === count_chars(strtolower($toCompare), 1));
 }
-$value = is_twins('Onnaaann', 'Aaaonnnn');
+$value = is_twins('', 'Aaaonnnn');
 var_dump($value);
